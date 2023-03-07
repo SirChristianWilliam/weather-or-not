801dce7bc226ac710ac1359e0ac9a32f762f3cec
Chris Maki <c.william.developer@gmail.com>
2023-03-07 17:21:15 -0600

    Added some styling on this bad boy to make responsive

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
-
   return (
     <div className="App">
-      <h1> Initial Initial Initial </h1>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Pine City, MN</p>
+          </div>
+          <div className="temp">
+            <h1>60℉</h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+        </div>
+
+        <div className="bottom">
+          <div className="feels">
+            <p className="bold">65℉</p>
+            <p> Feels Like: </p>
+          </div>
+          <div className="humidity">
+            <p className="bold">20%</p>
+            <p>Humidity</p>
+          </div>
+          <div className="wind">
+            <p className="bold">12 MPH</p>
+            <p>Wind Speed</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
