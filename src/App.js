123c752fba13e9bae78977953ed3c0245c8d9f3e
Chris Maki <c.william.developer@gmail.com>
2023-03-09 11:51:18 -0600

    Changed .env variables to proper format to allow recognition, so you scoundrels can't steal my API key

--- src/App.js
+++ src/App.js
@@ -1,33 +1,57 @@
 import React, { useState } from "react";
 import axios from "axios";
 
+
 function App() {
+  const [DataTransfer, setData] = useState({});
+  const [location, setLocation] = useState("");
+  const url = `${process.env.REACT_APP_WEATHER_ENDPOINT_URL}?q=${location}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=imperial`;
+
+  const searchLocation = (event) => {
+    if (event.key === "Enter") {
+      axios.get(url).then((response) => {
+        setData(response.data);
+        console.log("response.data is", response.data);
+      })
+      setLocation('');
+    }
+  };
   return (
     <div className="App">
+      <div className="search">
+        <input
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          onKeyDown={searchLocation}
+          placeholder="Enter Location"
+          type="text"
+        />
+      </div>
       <div className="container">
         <div className="top">
           <div className="location">
-            <p>Pine City, MN</p>
+            <p>{DataTransfer.name}</p>
           </div>
           <div className="temp">
-            <h1>60℉</h1>
+            {DataTransfer.main ? <h1>{DataTransfer.main.temp}℉</h1> : null}
           </div>
           <div className="description">
-            <p>Clouds</p>
+          {DataTransfer.main ? <p>{DataTransfer.weather[0].description}</p> : null}
+
           </div>
         </div>
 
         <div className="bottom">
           <div className="feels">
-            <p className="bold">65℉</p>
+            {DataTransfer.main ? <p className="bold">{DataTransfer.main.feels_like}℉</p> : null}
             <p> Feels Like: </p>
           </div>
           <div className="humidity">
-            <p className="bold">20%</p>
+            {DataTransfer.main ? <p className="bold">{DataTransfer.main.humidity}%</p> : null}
             <p>Humidity</p>
           </div>
           <div className="wind">
-            <p className="bold">12 MPH</p>
+          {DataTransfer.main ? <p className="bold">{DataTransfer.wind.speed}mph</p> : null}
             <p>Wind Speed</p>
           </div>
         </div>
