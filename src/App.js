cb9d423be362b35f38c59f33fd793ed5ac3007b2
Chris Maki <c.william.developer@gmail.com>
2023-03-11 15:29:55 -0600

    Successfully connected the API on the client. This was just a spike to brush up on APIs and I got what I wanted out of it so I won't be hosting this as of right now. Project complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-
 function App() {
   const [DataTransfer, setData] = useState({});
   const [location, setLocation] = useState("");
